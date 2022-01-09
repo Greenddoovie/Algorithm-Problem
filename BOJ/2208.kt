b9eb1cbafaf1bce3e3e055ba09634c23ef684860
greenddoovie <mania01234@naver.com>
2022-01-10 02:27:12 +0900

    feat: 백준 2208번 풀이
    누적합을 이용해서 문제 풀이 진행
    
    S[b] - S[a]가 구간 합임을 이용함
    S[b]가 최대이고, S[a]가 최소이면 구간의 최대 값을 구할 수 있다.
    
    이 문제에서 S[b]는 유동적으로 변하므로, 각 b에 대해 최소 S[a]를 찾는 것이 중요
    
    따라서 Array에 누적합과 0 until 현재 인덱스에서 min 값을 가지고 있는다.
    따라서 S[a]를 고정적으로 알 수 있으므로, S[b]에 대해서 최대값을 구해주면 끝!
    
    추가로, 음수를 최대값으로 가질 수 있는데 안가져가는 경우 가치가 0이므로 음수보다 크다. 따라서 이 문제에서 정답의 범위는 >=0이 된다

---
+++ BOJ/2208.kt
@@ -0,0 +1,46 @@
+import kotlin.math.max
+
+class IO2208 {
+    private val br = System.`in`.bufferedReader()
+    private val bw = System.out.bufferedWriter()
+
+    fun getNM() = br.readLine().split(" ").map { it.toInt() }
+    fun getJewel() = br.readLine().toInt()
+    fun close() = bw.close()
+    fun write(message: String) = bw.write(message)
+}
+
+fun main() {
+    var answer = 0
+    val io = IO2208()
+    val (N, M) = io.getNM()
+
+    val jewels = Array(N + 1) { 0 }
+    var sum = 0
+
+    data class Node(val sum: Int, val min: Int)
+
+    val arr = Array(N + 1) { Node(0, 0) }
+
+    var min = Int.MAX_VALUE
+    repeat(N) {
+        val idx = it + 1
+        jewels[idx] = io.getJewel()
+        sum += jewels[idx]
+
+        if (sum < min) {
+            min = sum
+        }
+
+        arr[idx] = Node(sum, min)
+        if (idx >= M) {
+            answer = max (answer, max(arr[idx].sum, arr[idx].sum - arr[idx-M].min))
+        }
+    }
+    for (idx in M .. N) {
+        answer = max (answer, max(arr[idx].sum, arr[idx].sum - arr[idx-M].min))
+    }
+    io.write(answer.toString())
+    io.close()
+}
+
